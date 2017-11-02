f7f26eeafd3b17f89443496f7b2476c91995c5a5
Iustin Targovet <j_tirgov@live.com>
2017-11-02 20:29:32 +0000

    entries url

--- Website/Logbook/urls.py
+++ Website/Logbook/urls.py
@@ -3,4 +3,5 @@ from . import views
 app_name ='Logbook'
 urlpatterns = [
     url(r'^', views.IndexView.as_view(), name = 'index'),
+    url(r'^?P<log_id>[0-9]+)/$'), views.EntryView.as_view(), name = 'entry'),
 ]
